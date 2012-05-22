5fbcf88bc1901847b48bc39ffc71d45333bce156
Benjamin Valentin <benpicco@zedat.fu-berlin.de>
2012-05-22 14:40:08 +0200

    use batch adding of tiles to increase speed of test map generation

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,11 @@
+import java.util.LinkedList;
 import java.util.Random;
 
 import javax.annotation.Generated;
 
+import org.mapsforge.map.writer.model.TileData;
 import org.mapsforge.storage.tile.PCTilePersistenceManager;
+import org.mapsforge.storage.tile.TileDataContainer;
 import org.mapsforge.storage.tile.TilePersistenceManager;
 
 
@@ -10,17 +13,19 @@ public class Test {
 	
 	private static void generateTestFile(String file, int sizeX, int sizeY) {
 		TilePersistenceManager tpm = new PCTilePersistenceManager(file);
+		LinkedList<TileDataContainer> tdcll = new LinkedList<TileDataContainer>();
+		
 		Random generator = new Random(1337);
 		byte[] data = new byte[1024];
 		
 		for (int x = 0; x < sizeX; ++x) {
 			for (int y = 0; y < sizeY; ++y) {
 				generator.nextBytes(data);
-				tpm.insertOrUpdateTile(data, x, y, (byte) 0);
-				System.out.print(".");
+				tdcll.add(new TileDataContainer(data, TileDataContainer.TILE_TYPE_VECTOR, x, y, (byte) 0));
 			}
-			System.out.println();
 		}
+		
+		tpm.insertOrUpdateTiles(tdcll);
 		
 		tpm.close();
 	}
